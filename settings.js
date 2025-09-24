b4e3d2b3aa967be0a90a1d7c8c549b17efcaf6d5
Coded0103 <125983409+Coded0103@users.noreply.github.com>
2025-09-24 21:53:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "knight Bot",
+  botOwner: 'Coded bot', // Your name
+  ownerNumber: '08139320119', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
